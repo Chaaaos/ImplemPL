3da951937f0cc8c2762c1765e463488b2cda3f11
Gaïa Loutchmia <gaia.loutchmia@ens-paris-saclay.fr>
2025-05-28 14:29:16 +0200

    Completed the normalisation part (lift functions and normalisation function) and corrected mistakes.

--- main.ml
+++ main.ml
@@ -38,16 +38,25 @@ type term =
 
 
 
-(** NORMALISATION. -- WIP *)
-(** Smart constructors. --WIP *)
+(** NORMALISATION. *)
+(** Preliminary definitions for smart constructors. *)
+let box_apply5 : ('a -> 'b -> 'c -> 'd -> 'e -> 'f)
+                 -> 'a box -> 'b box -> 'c box -> 'd box -> 'e box
+                 -> 'f box
+  = fun f t1 t2 t3 t4 t5
+  -> apply_box (box_apply2 (unbox (box_apply2 f t1 t2)) t3 t4) t5
+
+
+
+(** Smart constructors. *)
 (* Smart constructors are needed to work under binders,
  which is required to write the normalisation function. *)
 
 (* TYPES. *)
+(* Not useful here, they will be later. *)
 (* Base type. *)
-(* let _TyBool : s_ty box = *)
-(* Should I use the "var_new" Bindlib function ? *)
-     
+let _TyBool : s_ty box = box TyBool
+  
 (* Arrow type. *)
 let _TyArr : s_ty box -> s_ty box -> s_ty box =
   box_apply2 (fun a b -> TyArr(a,b))
@@ -84,26 +93,29 @@ let _TmProj2 : term box -> term box =
   box_apply (fun p -> TmProj2(p))
 
 (* Left Injection. *)
-let _TmPair : term box -> s_ty box -> term box =
+let _TmInL : term box -> s_ty box -> term box =
   box_apply2 (fun tL tyR -> TmInL(tL,tyR))
 (* Right Injection. *)
-let _TmPair : s_ty box -> term box -> term box =
+let _TmInR : s_ty box -> term box -> term box =
   box_apply2 (fun tyL tR -> TmInR(tyL,tR))
 (* Case constructor. *)
-(* TODO. *)
+let _TmCaseOf : term box
+                -> s_ty box -> (term, term) binder box
+                -> s_ty box -> (term, term) binder box -> term box =
+  box_apply5 (fun v ty1 v1 ty2 v2 -> TmCaseOf(v, ty1, v1, ty2, v2))
 
 (* Let constructor. *)
 let _TmLet : term box -> (term,term) binder box -> term box =
   box_apply2 (fun a f -> TmLet(a,f))
 
 
-(** Lifting functions. --TODO *)
+(** Lifting functions. *)
 (* Needed to define the normalisation function,
  to box subterms after normalising under a binder. *)
 
 let rec lift_ty : s_ty -> s_ty box = fun a ->
   match a with
-  | TyBool -> failwith "not yet defined"
+  | TyBool -> _TyBool
   | TyArr(a, b) -> _TyArr (lift_ty a) (lift_ty b)
   | TyProd(a, b) -> _TyProd (lift_ty a) (lift_ty b)
   | TySum(a, b) -> _TySum (lift_ty a) (lift_ty b)
@@ -113,17 +125,18 @@ let rec lift_te : term -> term box = fun t ->
   | TmVar(x) -> _TmVar x
   | TmAbs(a, t) -> _TmAbs (lift_ty a) (box_binder lift_te t)
   | TmApp(t, u) -> _TmApp (lift_te t) (lift_te u)
-  | TmPair(t1, t2) -> failwith "not yet defined"
-  | TmProj1(p) -> failwith "not yet defined"
-  | TmProj2(p) -> failwith "not yet defined"
-  | TmInL(tL, tyR) -> failwith "not yet defined"
-  | TmInR(tyL, tR) -> failwith "not yet defined"
-  | TmCaseOf(v, ty1, v1, ty2, v2) -> failwith "not yet defined"
-  | TmLet(m, n) -> failwith "not yet defined"
+  | TmPair(t1, t2) -> _TmPair (lift_te t1) (lift_te t2)
+  | TmProj1(p) -> _TmProj1 (lift_te p)
+  | TmProj2(p) -> _TmProj2 (lift_te p)
+  | TmInL(tL, tyR) -> _TmInL (lift_te tL) (lift_ty tyR)
+  | TmInR(tyL, tR) -> _TmInR (lift_ty tyL) (lift_te tR)
+  | TmCaseOf(v, ty1, v1, ty2, v2)
+    -> _TmCaseOf (lift_te v) (lift_ty ty1) (box_binder lift_te v1) (lift_ty ty2) (box_binder lift_te v2)
+  | TmLet(m, n) -> _TmLet (lift_te m) (box_binder lift_te n)
+(* failwith "not yet defined" *)
 
 
-
-(** Normalisation function. --TODO *)
+(** Normalisation function. *)
 
 let rec nf : term -> term = fun t ->
   match t with
@@ -131,9 +144,8 @@ let rec nf : term -> term = fun t ->
      t
 
   | TmAbs(ty, f) -> (* /!\ : Binds one term variable. *)
-     TmAbs(ty, f)
-  (* let (x,t) = unbind f in
-     TmAbs(a, unbox (bind_var x (lift_te (nf t)))) *)
+      let (x,e) = unbind f in
+      TmAbs(ty, unbox (bind_var x (lift_te (nf e))))
   | TmApp(t, u) ->
      let u' = nf u in
      begin
@@ -148,14 +160,14 @@ let rec nf : term -> term = fun t ->
      TmPair (t1', t2')
   | TmProj1(p) ->
      begin
-       match nf p with
-       | TmPair(t1, _) -> nf t1
+       match p with
+       | TmPair(t1, _) -> t1
        | p' -> TmProj1(p')
      end
   | TmProj2(p) -> 
      begin
-       match nf p with
-       | TmPair(_, t2) -> nf t2
+       match p with
+       | TmPair(_, t2) -> t2
        | p' -> TmProj2(p')
      end
 
@@ -167,10 +179,13 @@ let rec nf : term -> term = fun t ->
      TmInR(tyL, tR')
 
   | TmCaseOf(v, ty1, v1, ty2, v2) -> (* /!\ : Binds two term variables. *)
-     TmCaseOf(v, ty1, v1, ty2, v2)
+     let (x1, e1) = unbind v1 in
+     let (x2, e2) = unbind v2 in
+     TmCaseOf(nf v, ty1, unbox(bind_var x1 (lift_te (nf e1))), ty2,  unbox(bind_var x2 (lift_te (nf e2))))
 
   | TmLet(m, n) -> (* /!\ : Binds one term variable. *)
-     TmLet(m, n)
+     let (x, e) = unbind n in
+     TmLet(nf m, unbox(bind_var x (lift_te (nf e))))
 
 
 (** Example of types and terms. --TODO *)
